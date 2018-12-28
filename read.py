d39fdc6a96af307b700c1200c59e94c8c6d35656
Adam Zhang <q360465913@gmail.com>
2018-12-28 14:32:21 +0800

    add filtering func

--- read.py
+++ read.py
@@ -12,6 +12,12 @@ print('档案读取完了, 总共有', len(data), '笔资料')
 sum_len = 0
 for d in data:
 	sum_len += len(d)
-	print(sum_len)
 print('每一笔留言的平均长度为', sum_len/len(data), '个字。')
 
+new = []
+for d in data:
+	if len(d) < 100:
+		new.append(d)
+print('一共有', len(new), '笔留言长度小于100')
+print(new[0])
+print(new[1])
